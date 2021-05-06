33a3fb72b7dbabe5dc1160ae05b77ded990a263b
Ignat <ignativanov1996@mail.ru>
2021-05-06 13:49:58 +0300

    2021.05.06.1349
    Stage 1 complete.

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -1,1 +1,15 @@
-# write your code here
+import sys
+import os
+
+
+args = sys.argv  # we get the list of arguments
+
+if len(args) > 1:
+    # print(args[1])
+    for root, dirs, files in os.walk('.\\' + args[1], topdown=True):
+        for name in files:
+            print(os.path.join(root, name))
+        # for name in dirs:
+        #     print(os.path.join(root, name))
+else:
+    print('Directory is not specified')
